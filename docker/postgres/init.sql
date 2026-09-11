f67bd93200d15d5d94f81048dcc4baf98e3fc3eb
Piyush Jhunjhunwala <piyushjjw2004@gmail.com>
2026-09-11 11:31:17 +0530

    fix: align compose healthcheck and postgres init

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -1,6 +1,6 @@
 -- PostgreSQL container initialization. The official image creates POSTGRES_DB
 -- before executing this file, so do not issue CREATE DATABASE here.
-\connect taskflow_db;
+\connect taskflow;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
